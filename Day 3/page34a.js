cfa614acb4bee9788cdfd0472b039a7aa7c57a15
yogesh26-5 <yogeshkantheti@gmail.com>
2026-03-06 13:42:32 +0530

    Add files via upload

--- Day 3/page34a.js
+++ Day 3/page34a.js
@@ -3,6 +3,39 @@ const app=express()
 app.set("view engine","ejs");
 app.set("views","./views");
 app.listen(8080,()=> console.log("Server started"));
-app.get("/",(req,res)=>{
+app.use(express.urlencoded({extended:true}));
+let users=[
+    {name:"raghu", email:"raghu@gmail.com", password:"r4123"},
+    {name:"yogi", email:"yogesh@gmail.com", password:"n2123"},
+    {name:"lalith", email:"lalith@gmail.com", password:"p4123"},
+];
+app.get("/login",(req,res)=>{
+    res.render("login",{error:null});
+});
+
+app.post("/login",(req,res)=>{
+const {email,password}=req.body; //Destructuring
+const user=users.find((user)=> user.email===email);
+if (user){
+   if (user.password===password){
+      res.redirect("/dashboard");
+    }else{
+    res.render("login",{error:"Invalid password"});
+    }
+}else{
+    res.render("login",{error:"User not found"});
+}
+});
+
+app.get("/register",(req,res)=>{
     res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+    users=[...users,req.body];
+    res.redirect("/dashboard");
+});
+
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard",{users});
 });
